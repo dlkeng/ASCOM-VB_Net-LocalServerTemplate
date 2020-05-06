8632a89a9b876a2fdd0a4fd0d46f70255885fe93
Dan <dlkeng@msn.com>
2020-05-06 10:06:56 -0500

    VisualBasic Driver files

---
+++ ASCOM How-To/VisualBasic/ASCOM Driver/Acme/Focuser/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ASCOM Acme.Focuser")> 
+<Assembly: AssemblyDescription("ASCOM Focuser driver for Acme")> 
+<Assembly: AssemblyCompany("ASCOM Initiative")> 
+<Assembly: AssemblyProduct("ASCOM Focuser driver for Acme")> 
+<Assembly: AssemblyCopyright("Copyright © 2015, The ASCOM Intiative")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(True)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("77f919fc-c2c3-45f7-a06f-2dfad7e9a17c")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("6.0.0.0")> 
+<Assembly: AssemblyFileVersion("6.0.0.0")> 
